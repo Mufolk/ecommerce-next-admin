f8e04da5022aedb48fbfa2d1c98b0aadb833ddc0
mufolk <guilhermemelo138@gmail.com>
2023-06-08 20:47:43 -0300

    Revert "feat: implementing tabs css logic"
    This reverts commit 1e4064174f320e92b2ed9c374db8fa3ca3e320a1.

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,5 +1,4 @@
 // import Nav here and export it
 import { Nav } from "./Nav";
-import { Tab } from "./Tab";
 
-export { Nav, Tab };
+export { Nav };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
-import { Layout } from "../components/Layout";
+import { useSession, signIn } from "next-auth/react";
+import { Nav } from "../components/Nav";
 
-export default function Home() {
+export default function Component() {
+  const { data: session } = useSession();
+  if (typeof session === "undefined") {
+    return <div>Loading...</div>; // or any other appropriate loading state
+  }
+  if (session) {
+    return (
+      <div className="bg-blue-900 min-h-screen flex">
+        <Nav />
+        <div
+          className="bg-white text-black 
+                        flex-grow 
+                        mt-2 mr-2 mb-2 p-4 
+                        rounded-lg"
+        >
+          Logged in as {session.user.email}
+        </div>
+        {/* <button type="button" onClick={() => signOut()}>
+          Sign out
+        </button> */}
+      </div>
+    );
+  }
   return (
-    <Layout>
-      Test
-    </Layout>
-  )
+    <>
+      Not signed in <br />
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            type="button"
+            className="bg-white p-2 rounded-lg text-black"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }

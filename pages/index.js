190873c171560984b2ab011fbff3193d76758bb4
mufolk <guilhermemelo138@gmail.com>
2023-06-08 21:51:33 -0300

    fix: ajudsting image tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
 import Layout from "../components/Layout/Layout";
 
 export default function Component() {
@@ -12,7 +11,7 @@ export default function Component() {
           Hello, <b>{session?.user?.name}</b>
         </h2>
         <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
-          <Image src={session?.user?.image} alt="" className="w-6 h-6 " />
+          <img src={session?.user?.image} alt="" className="w-6 h-6 " />
           <span className="px-2">{session?.user?.name}</span>
         </div>
 
